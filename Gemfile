57f3c85599e22985f44ca8f684cace640fb19400
EC2 Default User <ec2-user@ip-172-31-21-74.us-east-2.compute.internal>
2019-12-02 04:16:20 +0000

    adding devise and user functionality

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'puma', '~> 3.11'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'devise'
+gem 'jbuilder'
+gem 'sdoc'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
+# Rails.application.routes.draw do
+#   devise_for :users
+#   # resources :campaigns
+#   namespace 'api' do
+#     namespace 'v1' do
+#       resources :campaigns
+#     end
+#   end
+# end
+
 Rails.application.routes.draw do
-  # resources :campaigns
-  namespace 'api' do
-    namespace 'v1' do
-      resources :campaigns
-    end
+  scope :api, defaults: { format: :json } do
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
+    resource :user, only: [:show, :update]
   end
 end
