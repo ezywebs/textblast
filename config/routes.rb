a6bb0e114a3102149a9b3a0aabc1a659cc8c25d6
EC2 Default User <ec2-user@ip-172-31-21-74.us-east-2.compute.internal>
2019-12-04 05:26:32 +0000

    welcome screen

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@
 # end
 
 Rails.application.routes.draw do
+  root 'welcome#index'
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions },
                        path_names: { sign_in: :login }
