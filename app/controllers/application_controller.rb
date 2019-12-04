e36f477198cd2e9c9a87338ee2b437137c20eab2
EC2 Default User <ec2-user@ip-172-31-21-74.us-east-2.compute.internal>
2019-12-04 05:35:22 +0000

    fix for index action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
     # protect_from_forgery with: :null_session
     respond_to :json
     
-    # before_action :underscore_params!
+    before_action :underscore_params!, except: :index
     before_action :authenticate_user
     
     private
